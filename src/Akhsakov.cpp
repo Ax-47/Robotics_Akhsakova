0e39676d1bc8bbd8a421794b10a8e3835ab87c0f
Ax-47 <armingLwnza@gmail.com>
2024-03-16 20:05:58 +0700

    oled init

--- src/Akhsakov.cpp
+++ src/Akhsakov.cpp
@@ -3,31 +3,32 @@
 #include <Adafruit_SSD1306.h>
 #include <Arduino.h>
 #include <Wire.h>
-ESP32Akhsakova::ESP32Akhsakova() {}
+ESP32Akhsakova::ESP32Akhsakova()
+    : oled{SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1} {}
 
 void ESP32Akhsakova::oledInit() {
-  Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
-  this->oled = oled;
   if (!oled.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println(F("failed to start SSD1306 OLED"));
     while (1)
       ;
   }
-  delay(2000);         // wait two seconds for initializing
-  oled.clearDisplay(); // clear display
-  oled.setTextSize(2);
-  oled.setTextColor(WHITE);
-  oled.setCursor(0, 10);
-  oled.println("Created by");
-  oled.display();
+  delay(2000);               // wait two seconds for initializing
+  this->oled.clearDisplay(); // clear display
+  this->oled.setTextSize(2);
+  this->oled.setTextColor(WHITE);
+  this->oled.setCursor(0, 10);
+  this->oled.println("Created by");
+  this->oled.display();
   delay(1000);
-  oled.clearDisplay();
-  oled.setCursor(0, 10);
-  oled.println("Mikhail\nAkhsakov");
-  oled.display();
+  this->oled.clearDisplay();
+  this->oled.setCursor(0, 10);
+  this->oled.println("Mikhail\nAkhsakov");
+  this->oled.display();
   delay(2000);
-  oled.clearDisplay();
-  oled.display();
+  this->oled.clearDisplay();
+  this->oled.display();
 }
-
-void ESP32Akhsakova::Begin() { this->oledInit(); }
+void ESP32Akhsakova::Begin() {
+  this->oledInit();
+  // this->akhsakov();
+}
--- src/Akhsakov.h
+++ src/Akhsakov.h
@@ -5,11 +5,9 @@
 #include "LightSensor/LightSensor.h"
 #include "Motor/Motor.h"
 #include "UltraSonicSensor/UltraSonicSensor.h"
-#include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include <Wire.h>
-#define SCREEN_WIDTH 128 // OLED width,  in pixels
-#define SCREEN_HEIGHT 64 // OLED height, in pixels
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
 
 class ESP32Akhsakova {
 public:
@@ -19,6 +17,8 @@ public:
 private:
   Adafruit_SSD1306 oled;
   void oledInit();
+
+  void akhsakov();
 };
 
 #endif
